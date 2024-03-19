6322407b22b6b217ed70a6adf8a2aebcdb3cf57f
theSudesh <xudes00@gmail.com>
2024-03-19 23:39:04 +0545

    First update

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    ##path('', views.comments, name='comments'),
+    # path('check-spam', views.check_spam, name='check_spam'),
+    path('check', views.check, name='check'),
+    # path('comments/<int:pk>/', views.delete_comment, name='delete-comment'),
+   
+   
+    path('', views.home, name="home"),
+    path('signup',views.signup, name ="signup"),
+    path('signin',views.signin, name ="signin"),
+    path('signout',views.signout, name ="signout"),
+    # path('check-spam',views.check_spam, name="check_spam"),
+]
